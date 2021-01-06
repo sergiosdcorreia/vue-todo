e183a6a1cb96c54b565ff1a0d824e47b986f3d6f
Sergio Correia <sergiosdcorreia@gmail.com>
2021-01-06 23:49:19 +0000

    Styling with scss preprocessor

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -84,14 +84,16 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
   @media screen and (min-width: 768px) {
     .q-footer {
       display: none;
     }
   }
 
-  .q-drawer .q-router-link--active {
-    color: white !important;
+  .q-drawer {
+    .q-router-link--active {
+      color: white !important;
+    }
   }
 </style>
